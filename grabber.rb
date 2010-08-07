d80e7992e9218383e30ef37d935bff58dc1365e6
Lloyd Pick <lloydpick@gmail.com>
2010-08-07 20:18:06 +0100

    initial grabber and printer

---
+++ grabber.rb
@@ -0,0 +1,104 @@
+print "Starting up... "
+require 'rubygems'
+require 'nokogiri'
+require 'mysql'
+require 'open-uri'
+puts "Done"
+
+print "Connecting to mySQL... "
+dbh = Mysql.real_connect("localhost", "root", "", "gatherer_db")
+puts "Done"
+
+print "Clearing old data... "
+dbh.query("TRUNCATE objects")
+puts "Done"
+
+# Mining nodes
+print "Fetching mining node types... "; $stdout.flush 
+mines = Nokogiri::HTML(open("http://www.wowhead.com/objects=-4"))
+puts "Done"
+
+print "  Extracting JS... "
+mines_js = mines.xpath('//script[@type="text/javascript"]')
+puts "Done"
+
+print "  Searching for correct JS... "
+listview_js = ""
+mines_js.each do |js|
+  if js.content[/Listview/x] != nil
+    listview_js = js.content
+  end
+end
+puts "Done"
+
+print "  Regexing JS... "
+  mines = listview_js.scan(/"id":(\d*),/i)
+puts "Done"
+
+
+# Herb nodes
+print "Fetching herb node types... "; $stdout.flush 
+herbs = Nokogiri::HTML(open("http://www.wowhead.com/objects=-3"))
+puts "Done"
+
+print "  Extracting JS... "
+herbs_js = herbs.xpath('//script[@type="text/javascript"]')
+puts "Done"
+
+print "  Searching for correct JS... "
+listview_js = ""
+herbs_js.each do |js|
+  if js.content[/Listview/x] != nil
+    listview_js = js.content
+  end
+end
+puts "Done"
+
+print "  Regexing JS... "
+  herbs = listview_js.scan(/"id":(\d*),/i)
+puts "Done"
+
+objects = mines | herbs
+
+objects.each do |object|
+  object_id = object
+
+  puts "Searching for #{object}... "
+
+  print "  Fetching object data... "; $stdout.flush 
+  data = Nokogiri::HTML(open("http://www.wowhead.com/object=#{object_id}"))
+  puts "Done"
+
+  print "  Extracting JS... "
+  all_js = data.xpath('//script[@type="text/javascript"]')
+  puts "Done"
+
+  print "  Searching for correct JS... "
+  mapper_js = ""
+  all_js.each do |js|
+    if js.content[/g_mapperData/x] != nil
+      mapper_js = js.content
+    end
+  end
+  puts "Done"
+
+  print "  Regexing JS... "
+  results = mapper_js.scan(/(\d*): \{\n\d*: \{ count: \d*, coords: \[(.*)\] \}\n\}/i)
+  puts "Done"
+
+  print "  Putting data into DB... "
+  results.each do |result|
+    coord_results = result[1].scan(/\[(\d*.\d*),(\d*.\d*)\]/)
+    coord_results.each do |coord|
+      coords = sprintf("%0.02f", (coord[0].to_f / 10)).to_s.gsub('.','') + sprintf("%0.02f", (coord[1].to_f / 10)).to_s.gsub('.','')
+      dbh.query("INSERT INTO objects (type_id, coords, zone) VALUES ('#{object_id}', '#{coords}', '#{result[0]}')")
+    end
+  end
+
+  puts "Done"
+
+end
+
+
+print "Cleaning up..."
+puts "Finished!"
---
+++ printer.rb
@@ -0,0 +1,130 @@
+require 'rubygems'
+require 'mysql'
+
+ZONES = {
+  # Kalimdor
+  "331" => 'ASHENVALE',
+  "16" => 'AZSHARA',
+  "3524" => 'AZUREMYST_ISLE',
+  "17" => 'BARRENS',
+  "3525" => 'BLOODMYST_ISLE',
+  "148" => 'DARKSHORE',
+  "1657" => 'DARNASSUS',
+  "405" => 'DESOLACE',
+  "14" => 'DUROTAR',
+  "15" => 'DUSTWALLOW_MARSH',
+  "361" => 'FELWOOD',
+  "357" => 'FERALAS',
+  "493" => 'MOONGLADE',
+  "215" => 'MULGORE',
+  "1637" => 'ORGRIMMAR',
+  "1377" => 'SILITHUS',
+  "406" => 'STONETALON_MOUNTAINS',
+  "440" => 'TANARIS',
+  "141" => 'TELDRASSIL',
+  "3557" => 'EXODAR',
+  "400" => 'THOUSAND_NEEDLES',
+  "1638" => 'THUNDER_BLUFF',
+  "490" => 'UNGORO_CRATER',
+  "618" => 'WINTERSPRING',
+
+  # Eastern Kingdoms
+  "36" => 'ALTERAC_MOUNTAINS',
+  "45" => 'ARATHI_HIGHLANDS',
+  "3" => 'BADLANDS',
+  "4" => 'BLASTED_LANDS',
+  "46" => 'BURNING_STEPPES',
+  "41" => 'DEADWIND_PASS',
+  "1" => 'DUN_MOROGH',
+  "10" => 'DUSKWOOD',
+  "139" => 'EASTERN_PLAGUELANDS',
+  "12" => 'ELWYNN_FOREST',
+  "3430" => 'EVERSONG_WOODS',
+  "3433" => 'GHOSTLANDS',
+  "267" => 'HILLSBRAD_FOOTHILLS',
+  "47" => 'HINTERLANDS',
+  "1537" => 'IRONFORGE',
+  "38" => 'LOCH_MODAN',
+  "44" => 'REDRIDGE_MOUNTAINS',
+  "51" => 'SEARING_GORGE',
+  "3487" => 'SILVERMOON',
+  "130" => 'SILVERPINE_FOREST',
+  "1519" => 'STORMWIND',
+  "33" => 'STRANGLETHORN_VALE',
+  "4080" => 'QUEL_DANAS',
+  "8" => 'SWAMP_OF_SORROWS',
+  "85" => 'TIRISFAL_GLADES',
+  "1497" => 'UNDERCITY',
+  "28" => 'WESTERN_PLAGUELANDS',
+  "40" => 'WESTFALL',
+  "11" => 'WETLANDS',
+
+  # Outland
+  "3522" => 'BLADES_EDGE_MOUNTAINS',
+  "3483" => 'HELLFIRE_PENINSULA',
+  "3518" => 'NAGRAND',
+  "3523" => 'NETHERSTORM',
+  "3520" => 'SHADOWMOON_VALLEY',
+  "3703" => 'SHATTRATH',
+  "3519" => 'TEROKKAR_FOREST',
+  "3521" => 'ZANGARMARSH',
+
+  # Northrend
+  "3537" => 'BOREAN_TUNDRA',
+  "2817" => 'CRYSTALSONG_FOREST',
+  "4395" => 'DALARAN',
+  "65" => 'DRAGONBLIGHT',
+  "394" => 'GRIZZLY_HILLS',
+  "495" => 'HOWLING_FJORD',
+  "4742" => 'HROTHGARS_LANDING',
+  "210" => 'ICECROWN_GLACIER',
+  "3711" => 'SHOLAZAR_BASIN',
+  "67" => 'STORM_PEAKS',
+  "4197" => 'LAKE_WINTERGRASP',
+  "66" => 'ZULDRAK'
+}
+
+def lookup_zone(id)
+  ZONES[id.to_s]
+end
+
+puts '-- This data is collected courtesy of Wowhead.com
+
+if not GathererDB.Wowhead.isLoading then return end
+
+GathererDB.Wowhead.data = {'
+
+dbh = Mysql.real_connect("localhost", "root", "", "gatherer_db")
+
+zones = dbh.query("SELECT DISTINCT(zone) FROM objects")
+
+while zone = zones.fetch_row do
+  unless lookup_zone(zone[0]).nil?
+    puts "\t#{lookup_zone(zone[0])} = {"
+    types = dbh.query("SELECT DISTINCT(type_id) FROM objects WHERE zone = '#{zone[0]}' ORDER BY type_id ASC")
+
+    while type = types.fetch_row do
+      puts "\t\t[#{type[0]}] = {"
+      print "\t\t\t"
+
+      objects = dbh.query("SELECT coords FROM objects WHERE zone = '#{zone[0]}' AND type_id = '#{type[0]}'")
+      count = 0
+      while object = objects.fetch_row do
+        print "#{object[0]},"
+
+        if count == 8
+          print "\n\t\t\t"
+          count = 0
+        else
+          count = count + 1
+        end
+      end
+   
+      puts "\n\t\t},"
+    end
+
+    puts "\t},"
+  end
+end
+
+puts "}"
